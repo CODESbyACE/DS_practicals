19b16ce7a55bcc225d7ea3e9701839deebc9696c
CODESbyACE <codesbyace@gmail.com>
2023-10-29 21:36:07 +0530

    Stack.cpp updated

--- stack.cpp
+++ stack.cpp
@@ -1,33 +1,83 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct node {
+struct node
+{
     int info;
     node *next;
 };
+node *top = NULL;
 
-struct node *front;
-struct node *rear;
+void push(int num)
+{
+    node *ptr;
+    ptr = new node;
+    ptr->info = num;
+    ptr->next = top;
+    top = ptr;
+}
+void pop()
+{
+    if (top == NULL)
+    {
+        cout << "Stack Underflow" << endl;
+    }
+    else
+    {
+        cout << "The popped element is: " << top->info << endl;
+        top = top->next;
+    }
+}
+void disp()
+{
+    node *temp = top;
+    cout << "Stack is: \n";
+    while (temp != NULL)
+    {
+        cout << temp->info << "\n";
+        temp = temp->next;
+    }
+}
 
-int main(){
-    node *t,*p, *z;
-    t = new node;
-    front = t;
-    rear = t;
-    t->info = 42;
-    p = new node;
-    t->next = p;
-    p->info = 55;
-    rear = p;
-    z = new node;
-    p->next = z;
-    z->info = 69;
-    rear = z;
-    int ptr = (t->next->info);
-    cout << "The value of the element is: "<< t->info << endl;
-    cout << "The value of the element is: "<< ptr<< endl;
-    cout << "The value of the element is: "<< z->info << endl;
+int main()
+{
+    while (1)
+    {
+        cout << "\n1. Push an item\n2. Pop an item\n3. Display Stack\n4. Exit\nYour choice ?\n";
+            int choice;
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+        {
+            int data;
+            cout << "Enter the value to be pushed in stack: ";
+            cin >> data;
+            push(data);
+            break;
+        }
+        case 2:
+        {
+            pop();
+            break;
+        }
+        case 3:
+        {
+            disp();
+            break;
+        }
 
-    delete t,p,z;
+        case 4:{
+            cout << "Exiting.....\n";
+            exit(0);
+        }
+        default:
+        {
+            cout<<"Invalid Choice\n";
+            break;
+        }
+        }
+    }
+
     return 0;
 }
